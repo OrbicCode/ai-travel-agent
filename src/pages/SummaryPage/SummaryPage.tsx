4d69b22fb0eea18d6049a012afbf4a5576a2cb72
James O'Kane <orbiccode@gmail.com>
2025-12-21 16:09:15 +0000

    added ai response

--- src/pages/SummaryPage/SummaryPage.tsx
+++ src/pages/SummaryPage/SummaryPage.tsx
@@ -6,13 +6,15 @@ export default function SummaryPage() {
 
   const { flyingFrom, flyingTo, fromDate, toDate, aiResponse } = state;
 
+  const { weather, flights, hotels } = JSON.parse(aiResponse);
+
   return (
     <div className={styles.container}>
       <h1>Your Trip</h1>
 
       <div className={styles.dateContainer}>
         <div>
-          <span className={styles.arrow}>⇨</span>
+          <span className={styles.arrow}>→</span>
           <span>
             {new Date(fromDate).toLocaleDateString('en-gb', {
               day: 'numeric',
@@ -29,27 +31,27 @@ export default function SummaryPage() {
               year: 'numeric',
             })}
           </span>
-          <span className={styles.arrow}>⇦</span>
+          <span className={styles.arrow}>←</span>
         </div>
       </div>
 
       <div className={styles.locationsContainer}>
         <span>{flyingFrom}</span>
-        <span className={styles.arrow}>⇨</span>
+        <span className={styles.arrow}>→</span>
         <span>{flyingTo}</span>
       </div>
 
       <div>
         <h2>Weather</h2>
         <div className={styles.resultsContainer}>
-          <span>{aiResponse}</span>
+          <span>{weather}</span>
         </div>
       </div>
 
       <div>
         <h2>Flights</h2>
         <div className={styles.resultsContainer}>
-          <span>The best option for you is with Delta Airlines with a layover in Oslo </span>
+          <span>{flights}</span>
           <button className={styles.button}>Book</button>
         </div>
       </div>
@@ -57,7 +59,7 @@ export default function SummaryPage() {
       <div>
         <h2>Hotels</h2>
         <div className={styles.resultsContainer}>
-          <span>We recommend you stay at the Premiere Inn hotel in central Paris</span>
+          <span>{hotels}</span>
           <button className={styles.button}>Book</button>
         </div>
       </div>
