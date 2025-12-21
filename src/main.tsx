6d8d0936895309c6d98531ac89aec90d14f04f95
James O'Kane <orbiccode@gmail.com>
2025-12-21 02:00:24 +0000

    added loading and summary routes

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import './index.css';
 import LandingPage from './pages/LandingPage/LandingPage';
 import PlanningPage from './pages/PlanningPage/PlanningPage';
+import LoadingPage from './pages/LoadingPage/LoadingPage';
+import SummaryPage from './pages/SummaryPage/SummaryPage';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +13,8 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='planning' element={<PlanningPage />} />
+        <Route path='loading' element={<LoadingPage />} />
+        <Route path='summary' element={<SummaryPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
