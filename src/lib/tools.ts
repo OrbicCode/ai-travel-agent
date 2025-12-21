b546e47b89765864f99bfdb561333f9074e3671e
James O'Kane <orbiccode@gmail.com>
2025-12-21 02:01:07 +0000

    init tools

---
+++ src/lib/tools.ts
@@ -0,0 +1,50 @@
+// type Weather = {
+//   description: string;
+//   tempMinKelvin: number;
+//   tempMaxKelvin: number;
+// };
+
+export async function getWeather(flightTo: string) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${flightTo}&appid=72981ed094d399cae0ff841f806f4508`
+    );
+    const data = await response.json();
+    if (data) {
+      const weather = {
+        description: data.weather[0].description,
+        tempMinKelvin: data.main.temp_min,
+        tempMaxKelvin: data.main.temp_max,
+      };
+      return JSON.stringify(weather);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+// export async function getFlights(travellers, flightFrom, flightTo) {}
+
+// export async function getHotel(travellers, flightTo) {}
+
+export const tools = [
+  {
+    type: 'function' as const,
+    function: {
+      name: 'getWeather',
+      description:
+        'An api call to OpenWeather API that returns the weather description, minimum and maximum temperature in Kelvin.',
+      parameters: {
+        type: 'object',
+        properties: {
+          flightTo: {
+            type: 'string',
+            description: 'The destination, where the travellers are flying to.',
+          },
+        },
+        required: ['flightTo'],
+      },
+    },
+  },
+];
